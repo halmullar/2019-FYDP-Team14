37dcf5ae1b1ce9379d32edbe1b85b9bc25920438
JuanaAtt <j2attieh@edu.uwaterloo.ca>
2019-03-11 15:50:26 -0400

    Upload

---
+++ uploadd.php
@@ -0,0 +1,26 @@
+
+
+<?php  
+$connect = mysqli_connect("localhost", "username", "password", fydp);
+if(isset($_POST["submit"]))
+{
+ if($_FILES['file']['name'])
+ {
+  $filename = explode(".", $_FILES['file']['name']);
+  if($filename[1] == 'csv')
+  {
+   $handle = fopen($_FILES['file']['tmp_name'], "r");
+   while($data = fgetcsv($handle))
+   {
+    $item1 = mysqli_real_escape_string($connect, $data[0]);  
+                $item2 = mysqli_real_escape_string($connect, $data[1]);
+                $query = "INSERT into RawData2(ActualProduction, Cavity, Closingblow, Closingshot, Date, DownTime, GoodMaterial, Machine, OEE, Openingblow, Openingshot, Performance by Product, PlannedProduction, Product, Purge, QualitybyProduct, Reason, Shift, WasteMaterial, Weight, availability, cycletime, goodbottles, numbags, qauntitybag, rejectedbottles, rejectedperform, runninghours, totalblow, totalshot, wip) 
+                Values ('". $toInsert[8] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] "');
+                mysqli_query($connect, $query);
+   }
+   fclose($handle);
+   echo "<script>alert('Import done');</script>";
+  }
+ }
+}
+?> 
