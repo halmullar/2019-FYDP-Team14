8a169dd9b70c0451b576b6433d3219ede7ff7912
JuanaAtt <j2attieh@edu.uwaterloo.ca>
2019-03-18 19:16:42 -0400

    update

--- uploadd.php
+++ uploadd.php
@@ -5,31 +5,45 @@ $password = "password";
 
 // Create connection
 $connect = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($connect->connect_error) {
-    die("Connection failed: " . $connect->connect_error);
-} 
-echo "Connected successfully";
-if(isset($_POST["submit"]))
-{
- if($_FILES['file']['name'])
- {
-  $filename = explode(".", $_FILES['file']['name']);
-  if($filename[1] == 'csv')
-  {
-   $handle = fopen($_FILES['file']['tmp_name'], "r");
-   while($data = fgetcsv($handle))
-   {
-    $item1 = mysqli_real_escape_string($connect, $data[0]);  
-                $item2 = mysqli_real_escape_string($connect, $data[1]);
-                $query = "INSERT into RawData2(Date, Shift, Product, Weight, Machine, Cavity, Openingshot, Closingshot, Openingblow, Closingblow, numbags, quantitybag, wip, Purge, cycletime, runninghours, totalshot, totalblow, goodbottles, rejectedperform, rejectedbottles, GoodMaterial, WasteMaterial, PlannedProduction, ActualProduction, Performance by Product, QualitybyProduct, availability, OEE, DownTime, Reason)  
-                Values ('". $toInsert[8] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[2] . "','" . $toInsert[10] . "','" . $toInsert[10] "');
-                mysqli_query($connect, $query);
-   }
-   fclose($handle);
-   echo "<script>alert('Import done');</script>";
-  }
- }
+if ($connect) {
+  echo 'conected';
+} else {
+  echo 'not conected';
 }
+
+if(isset($_POST["Import"])){
+    
+    $filename=$_FILES["file"]["tmp_name"];    
+
+
+     if($_FILES["file"]["size"] > 0)
+     {
+        $file = fopen($filename, "r");
+          while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
+           {
+
+
+             $sql = "INSERT into RawData3(Date, Shift, Product, Weight, Machine, Cavity, Openingshot, Closingshot, Openingblow, Closingblow, numbags, quantitybag, wip, Purge, cycletime, runninghours, DownTime, Reason, totalshot, totalblow, goodbottles, rejectedperform, rejectedbottles, GoodMaterial, WasteMaterial, PlannedProduction, ActualProduction, Performance by Product, QualitybyProduct, availability, OEE)  
+                Values ('". $toInsert[8] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] "');
+                mysqli_query($connect, $query);
+                   $result = mysqli_query($con, $sql);
+        if(!isset($result))
+        {
+          echo "<script type=\"text/javascript\">
+              alert(\"Invalid File:Please Upload CSV File.\");
+              window.location = \"index.php\"
+              </script>";   
+        }
+        else {
+            echo "<script type=\"text/javascript\">
+            alert(\"CSV File has been successfully Imported.\");
+            window.location = \"index.php\"
+          </script>";
+        }
+           }
+      
+           fclose($file); 
+     }
+  }  
+
 ?> 
