5e9ebf2608e0af7e4d6b22c212cbd5fa45e88958
JuanaAtt <j2attieh@edu.uwaterloo.ca>
2019-03-19 14:26:36 -0400

    update

--- uploadd.php
+++ uploadd.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
+<html>
 <?php  
 $servername = "35.237.124.28";
 $username = "root";
-$password = "password";
+$password = "3inawi4e";
 
 // Create connection
 $connect = new mysqli($servername, $username, $password);
@@ -11,35 +13,32 @@ if ($connect) {
   echo 'not connected';
 }
 
-if(isset($_POST["Import"])){
-    
-    $filename=$_FILES["file"]["tmp_name"];    
+if(isset($_POST["Import"]) && isset($_FILES["fileToUpload"])){
+
+    $filename=$_FILES["fileToUpload"]["tmp_name"];    
 
 
-     if($_FILES["file"]["size"] > 0)
+     if($_FILES["fileToUpload"]["size"] > 0)
      {
         $file = fopen($filename, "r");
           while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
            {
 
+
              $sql = "INSERT into RawData3(Date, Shift, Product, Weight, Machine, Cavity, Openingshot, Closingshot, Openingblow, Closingblow, numbags, quantitybag, wip, Purge, cycletime, runninghours, DownTime, Reason, totalshot, totalblow, goodbottles, rejectedperform, rejectedbottles, GoodMaterial, WasteMaterial, PlannedProduction, ActualProduction, Performance by Product, QualitybyProduct, availability, OEE)  
-                
-
-
-                Values ('" . $toInsert[8] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] ."'")";
-                mysqli_query($connect, $query);
-                   $result = mysqli_query($con, $sql);
+                Values ('" . $toInsert[8] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "','" . $toInsert[10] . "')";
+                   $result = mysqli_query($connect, $sql);
         if(!isset($result))
         {
           echo "<script type=\"text/javascript\">
               alert(\"Invalid File:Please Upload CSV File.\");
-              window.location = \"index.php\"
+              window.location = \"UploadPage.html\"
               </script>";   
         }
         else {
             echo "<script type=\"text/javascript\">
             alert(\"CSV File has been successfully Imported.\");
-            window.location = \"index.php\"
+            window.location = \"UploadPage.html\"
           </script>";
         }
            }
@@ -49,3 +48,5 @@ if(isset($_POST["Import"])){
   }  
 
 ?> 
+</html>
+
